b85c9fd56ba6167d4682e36094fd49d283356e8a
Sahadat Khan <sahadat2004@outlook.com>
2026-02-11 03:05:44 +0600

    Refactor login function to simplify user validation

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -33,19 +33,27 @@ export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    const isMatch = await bcryptjs.compare(password, user.password);
-    if (!user || !isMatch) {
+
+    // ১. প্রথমে চেক করতে হবে ইউজার আছে কি না
+    if (!user) {
       return res.status(400).json({ message: "Invalid username or password" });
-    } else {
-      return res.status(200).json({
-        message: "Login successfull",
-        user: {
-          _id: user._id,
-          fullname: user.fullname,
-          email: user.email,
-        },
-      });
     }
+
+    // ২. ইউজার থাকলে তবেই পাসওয়ার্ড চেক করতে হবে
+    const isMatch = await bcryptjs.compare(password, user.password);
+    
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid username or password" });
+    }
+
+    return res.status(200).json({
+      message: "Login successful",
+      user: {
+        _id: user._id,
+        fullname: user.fullname,
+        email: user.email,
+      },
+    });
   } catch (error) {
     console.log("Error: " + error.message);
     return res.status(500).json({ message: "Internal server error" });
